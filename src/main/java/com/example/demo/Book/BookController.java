a0cf0df977cda54b274a33c0129ff3bed139ab02
Kavin-Java-Developer <kavink.s.it.2020@snsct.org>
2023-08-15 12:31:15 +0530

    modified

--- src/main/java/com/example/demo/Book/BookController.java
+++ src/main/java/com/example/demo/Book/BookController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequestMapping(path = "book")
@@ -21,10 +22,35 @@ public String test(){
 public List<Book> getBooks(){
         return books;
 }
+
+@GetMapping("/info/{id}")
+public Book getBookInfo(@PathVariable Long id){
+
+       for (Book book:books){
+           if (Objects.equals(book.id,id)){
+               return book;
+           }
+       }
+       return new Book();
+}
+
 @PostMapping("/add")
 public Book addBook(@RequestBody Book bookdata){
-        books.add(bookdata);
-        return bookdata;
+        Book newBook=new Book(bookdata.name,bookdata.author,bookdata.quantity,bookdata.releasedYear);
+        books.add(newBook);
+        return newBook;
 
+}
+
+@PutMapping("/update/{id}")
+public Book updateBook(@PathVariable Long id,@RequestBody Book bookData){
+for (int i=0;i<books.size();i++){
+    if (Objects.equals(books.get(i).id,id)){
+        Book updatedBook=bookData;
+        updatedBook.id=id;
+        books.set(i,updatedBook);
+    }
+}
+return bookData;
 }
 }
